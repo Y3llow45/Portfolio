68fec9113a6c84891fa9f161441a2f289a03a057
Борислав Христов <hristovborislav45@gmail.com>
2024-12-27 10:35:51 +0200

    Add clouds

--- interactive-cityscape/src/components/Cityscape/Cityscape.tsx
+++ interactive-cityscape/src/components/Cityscape/Cityscape.tsx
@@ -4,8 +4,8 @@ import Building from '../Building/Building';
 const Cityscape: React.FC = () => {
   return (
     <div className="cityscape">
-      <img src="/images/cloud.png" alt="cloud" className="cloud" style={{ animationDelay: '0s' }} />
-      <img src="/images/cloud.png" alt="cloud" className="cloud" style={{ animationDelay: '10s' }} />
+      <img src="/images/clouds.png" alt="cloud" className="cloud" style={{ animationDelay: '0s' }} />
+      <img src="/images/cloud2.jpg" alt="cloud" className="cloud" style={{ animationDelay: '10s' }} />
       <Building label="Front-End" imageSrc="/images/frontend.png" />
       <Building label="Back-End" imageSrc="/images/backend.png" />
       <Building label="Cybersecurity" imageSrc="/images/cybersecurity.png" />
