3200c0f0b5120d91f73f2690f49256fc304da8f2
Борислав Христов <hristovborislav45@gmail.com>
2024-12-27 13:36:19 +0200

    Update clouds

--- interactive-cityscape/src/components/Cityscape/Cityscape.tsx
+++ interactive-cityscape/src/components/Cityscape/Cityscape.tsx
@@ -4,8 +4,15 @@ import Building from '../Building/Building';
 const Cityscape: React.FC = () => {
   return (
     <div className="cityscape">
-      <img src="/images/clouds.png" alt="cloud" className="cloud" style={{ animationDelay: '0s' }} />
-      <img src="/images/cloud2.jpg" alt="cloud" className="cloud" style={{ animationDelay: '10s' }} />
+      <img src="/images/clouds1.png" alt="cloud" className="cloud" style={{ animationDelay: '0s' }} />
+      <img src="/images/clouds2.png" alt="cloud" className="cloud" style={{ animationDelay: '2s' }} />
+      <img src="/images/clouds3.png" alt="cloud" className="cloud" style={{ animationDelay: '4s' }} />
+      <img src="/images/clouds4.png" alt="cloud" className="cloud" style={{ animationDelay: '6s' }} />
+      <img src="/images/clouds5.png" alt="cloud" className="cloud" style={{ animationDelay: '8s' }} />
+      <img src="/images/clouds6.png" alt="cloud" className="cloud" style={{ animationDelay: '10s' }} />
+      <img src="/images/clouds7.png" alt="cloud" className="cloud" style={{ animationDelay: '12s' }} />
+      <img src="/images/clouds8.png" alt="cloud" className="cloud" style={{ animationDelay: '14s' }} />
+      <img src="/images/clouds9.png" alt="cloud" className="cloud" style={{ animationDelay: '16s' }} />
       <Building label="Front-End" imageSrc="/images/frontend.png" />
       <Building label="Back-End" imageSrc="/images/backend.png" />
       <Building label="Cybersecurity" imageSrc="/images/cybersecurity.png" />
