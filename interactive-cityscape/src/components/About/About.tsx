ac86af5903d62201aefd581ed330aeecdbd7fc82
Борислав Христов <hristovborislav45@gmail.com>
2025-01-01 20:42:48 +0200

    Add copy button with image and style

--- interactive-cityscape/src/components/About/About.tsx
+++ interactive-cityscape/src/components/About/About.tsx
@@ -10,7 +10,8 @@ const About = () => {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
       >
-        I'm a Full Stack Developer who likes building innovative projects. I'm skilled at creating good looking designs. Writing the back-end logic and solving problems is what I enjoy the most. I'm always curious to learn new tools and technologies, so that I can improve my skills and build better projects. I like to turn ideas into something real that people can use. If you think I would be a great fit for you team, feel free to email me at: <span className='selectable-text'>hristovborislav45@gmail.com</span>. 
+        I'm a Full Stack Developer who likes building innovative projects. I'm skilled at creating good looking designs. Writing the back-end logic and solving problems is what I enjoy the most. I'm always curious to learn new tools and technologies, so that I can improve my skills and build better projects. I like to turn ideas into something real that people can use. If you think I would be a great fit for you team, feel free to email me at: <span className='selectable-text'>hristovborislav45@gmail.com</span> 
+        <button className="btn-copy"><img className='btn-img' src='/images/copy.png'></img></button>
       </motion.p>
     </section>
   );
