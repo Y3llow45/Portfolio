2077fb44a7c09b65ab86ab654c4bdfb266fb1ccd
Борислав Христов <hristovborislav45@gmail.com>
2025-01-02 23:32:43 +0200

    Update Projects.tsx

--- interactive-cityscape/src/components/Projects/Projects.tsx
+++ interactive-cityscape/src/components/Projects/Projects.tsx
@@ -2,24 +2,24 @@ import { motion } from 'framer-motion';
 import './Projects.scss';
 
 const projectsData = [
-  { id: 1, title: 'Learn Electronics', description: 'A full-stack e-commerce solution built with React and Node.js.' },
-  { id: 2, title: 'Chato', description: 'A productivity app created using Vue.js and Firebase.' },
-  { id: 3, title: 'Steganography', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
-  { id: 4, title: 'Tic-tac-toe cheat', description: 'A full-stack e-commerce solution built with React and Node.js.' },
-  { id: 5, title: 'Sixty-six game', description: 'A productivity app created using Vue.js and Firebase.' },
-  { id: 6, title: 'URL-shortener-with-analytics', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
-  { id: 7, title: 'small-clock-arduino', description: 'A full-stack e-commerce solution built with React and Node.js.' },
-  { id: 8, title: 'discord-bots', description: 'A productivity app created using Vue.js and Firebase.' },
-  { id: 9, title: 'DrawingBe', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
-  { id: 10, title: 'Break-reminder', description: 'A full-stack e-commerce solution built with React and Node.js.' },
-  { id: 11, title: 'Cheat-for-perfect-circle', description: 'A productivity app created using Vue.js and Firebase.' },
-  { id: 12, title: 'IR-Remote-leds-and-receiver-with-Arduino', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
+  { id: 1, title: 'Learn Electronics', description: 'Learn about electrical components and how circuits work. You can learn or teach others by making lessons.' },
+  { id: 2, title: 'Chato', description: 'Encrypted real-time chat app, built for secure communication with a focus on privacy and speed.' },
+  { id: 3, title: 'Steganography', description: 'Desktop app for hiding secrete messages in images using Steganography.' },
+  { id: 4, title: 'Tic-tac-toe cheat', description: `Automated cheat for google's tic-tac-toe game.` },
+  { id: 5, title: 'Sixty-six game', description: 'Website for multiplayer cards game.' },
+  { id: 6, title: 'URL shortener with analytics', description: 'Website for shortening urls, monitoring visitors and generating qr codes.' },
+  { id: 7, title: 'Small clock arduino', description: 'Small 4 digit 7 segment clock using time module and arduino.' },
+  { id: 8, title: 'Discord bot', description: 'Discord bot that greets users, solves rubiks cubes and sudokus, send bash commands to server and much more.' },
+  { id: 9, title: 'DrawingBe', description: 'Desktop app that draws cool things using turtle library.' },
+  { id: 10, title: 'Break reminder', description: 'Desktop app that reminds you to take a break after 30 mins of work.' },
+  { id: 11, title: 'Cheat for perfect circle', description: 'Cheat for perfect circle game with ~98% accuracy.' },
+  { id: 12, title: 'IR-Remote LEDs with Arduino', description: 'Controll LEDs using IR remote.' },
 ];
 
 const Projects = () => {
   return (
     <section className="projects">
-      <h2>My Projects</h2>
+      <h2>My top 12 projects (38 total)</h2>
       <div className="projects-grid">
         {projectsData.map((project, index) => (
           <motion.div
