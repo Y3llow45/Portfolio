8d0e8c4b2cc0c1ddb8a452c7539e282f5504ac04
Борислав Христов <hristovborislav45@gmail.com>
2025-01-02 19:01:10 +0200

    Update Projects.tsx

--- interactive-cityscape/src/components/Projects/Projects.tsx
+++ interactive-cityscape/src/components/Projects/Projects.tsx
@@ -2,9 +2,18 @@ import { motion } from 'framer-motion';
 import './Projects.scss';
 
 const projectsData = [
-  { id: 1, title: 'E-commerce Platform', description: 'A full-stack e-commerce solution built with React and Node.js.' },
-  { id: 2, title: 'Task Management App', description: 'A productivity app created using Vue.js and Firebase.' },
-  { id: 3, title: 'Weather Forecast Dashboard', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
+  { id: 1, title: 'Learn Electronics', description: 'A full-stack e-commerce solution built with React and Node.js.' },
+  { id: 2, title: 'Chato', description: 'A productivity app created using Vue.js and Firebase.' },
+  { id: 3, title: 'Steganography', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
+  { id: 4, title: 'Tic-tac-toe cheat', description: 'A full-stack e-commerce solution built with React and Node.js.' },
+  { id: 5, title: 'Sixty-six game', description: 'A productivity app created using Vue.js and Firebase.' },
+  { id: 6, title: 'URL-shortener-with-analytics', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
+  { id: 7, title: 'small-clock-arduino', description: 'A full-stack e-commerce solution built with React and Node.js.' },
+  { id: 8, title: 'discord-bots', description: 'A productivity app created using Vue.js and Firebase.' },
+  { id: 9, title: 'DrawingBe', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
+  { id: 10, title: 'Break-reminder', description: 'A full-stack e-commerce solution built with React and Node.js.' },
+  { id: 11, title: 'Cheat-for-perfect-circle', description: 'A productivity app created using Vue.js and Firebase.' },
+  { id: 12, title: 'IR-Remote-leds-and-receiver-with-Arduino', description: 'An interactive weather dashboard using React and OpenWeatherMap API.' },
 ];
 
 const Projects = () => {
