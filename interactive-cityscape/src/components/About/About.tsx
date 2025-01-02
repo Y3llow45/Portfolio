1230c7156e29843733fb81679b31b159741c57f0
Борислав Христов <hristovborislav45@gmail.com>
2025-01-02 09:51:41 +0200

    Add copy to clipboard function

--- interactive-cityscape/src/components/About/About.tsx
+++ interactive-cityscape/src/components/About/About.tsx
@@ -2,6 +2,10 @@ import { motion } from 'framer-motion';
 import './About.scss';
 
 const About = () => {
+  const copy = () => {
+    navigator.clipboard.writeText('hristovborislav45@gmail.com');
+  }
+
   return (
     <section className="about">
       <h2>About Me</h2>
@@ -11,7 +15,7 @@ const About = () => {
         transition={{ duration: 0.8 }}
       >
         I'm a Full Stack Developer who likes building innovative projects. I'm skilled at creating good looking designs. Writing the back-end logic and solving problems is what I enjoy the most. I'm always curious to learn new tools and technologies, so that I can improve my skills and build better projects. I like to turn ideas into something real that people can use. If you think I would be a great fit for you team, feel free to email me at: <span className='selectable-text'>hristovborislav45@gmail.com</span> 
-        <button className="btn-copy"><img className='btn-img' src='/images/copy.png'></img></button>
+        <button className="btn-copy" onClick={() => copy()}><img className='btn-img' src='/images/copy.png'></img></button>
       </motion.p>
     </section>
   );
