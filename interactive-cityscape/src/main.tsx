566b083b36d4bea80caa1bbc23c7962295ba65e8
Y3llow45 <hristovborislav45@gmail.com>
2026-01-11 21:14:49 +0200

    Clean up from previous GA logic

--- interactive-cityscape/src/main.tsx
+++ interactive-cityscape/src/main.tsx
@@ -1,24 +1,10 @@
-import { StrictMode, useEffect, useRef } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App'
-import { pageview } from './ga'
-
-function Root() {
-  const hasTrackedInitial = useRef(false);
-
-  useEffect(() => {
-    if (!hasTrackedInitial.current) {
-      pageview(window.location.pathname + window.location.search);
-      hasTrackedInitial.current = true;
-    }
-  }, []);
-
-  return <App />
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Root />
+    <App />
   </StrictMode>
 )
